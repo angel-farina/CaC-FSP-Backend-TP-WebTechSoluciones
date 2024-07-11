aa9c2716471ef6ea6452d16321652e906f322c6f
angel-farina <zzyzxxrd@gmail.com>
2024-07-10 22:29:01 -0300

    added allowed host

--- contact_api/contact_api/settings.py
+++ contact_api/contact_api/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [#'127.0.0.1',
                  #'localhost',
                  'webtechsoluciones.netlify.app',
+                 'cac-fsp-backend-tp-webtechsoluciones.onrender.com',
                  ]
 
 # Application definition
